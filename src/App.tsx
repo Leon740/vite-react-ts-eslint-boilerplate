29de1311c560236422ed6e4739229c3ac6dae650
Leon740 <dobrinovleonid@gmail.com>
2024-11-13 17:31:22 -0500

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <button>btn</button>
+      <button>button</button>
       <div className="card">
         <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
